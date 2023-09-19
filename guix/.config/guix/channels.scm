37e15227b0671651466f3950fbecbee4c20ac81f
ROCKTAKEY <rocktakey@gmail.com>
2023-09-20 08:46:58 +0900

    Add ccwl channel

--- guix/.config/guix/channels.scm
+++ guix/.config/guix/channels.scm
@@ -15,6 +15,15 @@
         (name 'guix-hpc-non-free)
         (url "https://gitlab.inria.fr/guix-hpc/guix-hpc-non-free.git"))
        (channel
+        (name 'ccwl)
+        (url "https://github.com/arunisaac/ccwl")
+        (branch "main")
+        (introduction
+         (make-channel-introduction
+          "01ec2e0c15a9f7db2b6374e1469a90df30109300"
+          (openpgp-fingerprint
+           "7F73 0343 F2F0 9F3C 77BF  79D3 2E25 EE8B 6180 2BB3"))))
+       (channel
         (name 'roquix)
         (url "https://github.com/ROCKTAKEY/roquix"))
        %default-channels)
