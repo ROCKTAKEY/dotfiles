8cfeace2a894f26704ee984f9027303049f5a645
ROCKTAKEY <rocktakey@gmail.com>
2024-11-15 01:25:01 +0900

    guix-system: Remove nss-certs

--- guix-system/config/cuirass.scm
+++ guix-system/config/cuirass.scm
@@ -58,12 +58,6 @@
                   (supplementary-groups '("wheel" "netdev" "audio" "video")))
                 %base-user-accounts))
 
-  ;; Packages installed system-wide.  Users can also install packages
-  ;; under their own account: use 'guix search KEYWORD' to search
-  ;; for packages and 'guix install PACKAGE' to install a package.
-  (packages (append (list (specification->package "nss-certs"))
-                    %base-packages))
-
   ;; Below is the list of system services.  To search for available
   ;; services, run 'guix system search KEYWORD' in a terminal.
   (services
