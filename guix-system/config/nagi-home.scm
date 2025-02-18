91e9c9bf9b13b0689c50414e790fbc6b21f38c4f
ROCKTAKEY <rocktakey@gmail.com>
2025-02-19 00:45:40 +0900

    guix-system: Add home manager configuration for xremap

---
+++ guix-system/config/nagi-home.scm
@@ -0,0 +1,45 @@
+(use-modules (gnu home)
+             (gnu home services)
+             (gnu home services desktop)
+             (gnu home services shepherd)
+             (gnu services)
+             (gnu services configuration)
+             (gnu packages admin)
+             (guix gexp)
+             (gnu packages rust-apps))
+
+
+(define-configuration/no-serialization home-xremap-configuration
+  (config-file
+   (file-like)
+   "Path to config.yml file for xremap."))
+
+(define (xremap-shepherd-service config)
+  (list
+   (shepherd-service
+    (provision '(xremap))
+    (start #~(make-system-constructor
+              (string-join
+               (list #$(file-append xremap-x11 "/bin/xremap")
+                     "--watch=device,config" #$(home-xremap-configuration-config-file config)))))
+    (documentation "On startup, run @code{xmodmap} and read the expressions in
+the configuration file.  On stop, reset all the mappings back to the
+defaults."))))
+
+(define home-xremap-service-type
+  (service-type
+   (name 'home-xremap)
+   (extensions
+    (list
+     (service-extension home-shepherd-service-type
+                        xremap-shepherd-service)))
+   (description "Run the @code{xmodmap} utility to modify keymaps and pointer
+buttons under the Xorg display server via user-defined expressions.")))
+
+(home-environment
+  (services
+   (cons*
+    (service home-xremap-service-type
+             (home-xremap-configuration
+              (config-file (local-file "/home/rocktakey/.config/xremap/config.yml"))))
+    %base-home-services)))
