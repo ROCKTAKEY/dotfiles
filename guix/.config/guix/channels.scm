8e93d7556236d3d6531246f0efe3fa911d100ab3
ROCKTAKEY <rocktakey@gmail.com>
2025-06-12 22:26:49 +0900

    guix: Add guix-hpc

--- guix/.config/guix/channels.scm
+++ guix/.config/guix/channels.scm
@@ -11,6 +11,9 @@
         (name 'guix-jp)
         (url "https://gitlab.com/guix-jp/channel")
         (branch "main"))
+       (channel
+        (name 'guix-hpc)
+        (url "https://gitlab.inria.fr/guix-hpc/guix-hpc.git"))
        ;; (channel
        ;;  (name 'guix-hpc-non-free)
        ;;  (url "https://gitlab.inria.fr/guix-hpc/guix-hpc-non-free.git"))
