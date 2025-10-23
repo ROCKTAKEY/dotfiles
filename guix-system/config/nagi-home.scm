8f4da31c0114e7cf20dc38268661fb080cba8aa7
ROCKTAKEY <rocktakey@gmail.com>
2025-10-23 22:47:48 +0900

    xremap: Start xremap by shepherd

--- guix-system/config/nagi-home.scm
+++ guix-system/config/nagi-home.scm
@@ -1,8 +1,35 @@
 (use-modules (gnu home)
+             (gnu services)
              (gnu home services)
-             (guix gexp))
+             (gnu home services shepherd)
+             (guix gexp)
+             (gnu packages rust-apps))
 
 (home-environment
   (services
    (cons*
+    (service home-shepherd-service-type
+             (home-shepherd-configuration
+               (services
+                (list
+                 (shepherd-service
+                   (provision '(xremap))
+                   (documentation "xremap key remapping daemon")
+                   (auto-start? #t)
+                   (respawn? #t)
+                   (start #~(lambda ()
+                              (let* ((config (string-append (getenv "HOME")
+                                                            "/.config/xremap/config.yml")))
+                                (make-forkexec-constructor
+                                 (list #$(file-append xremap-wlroots "/bin/xremap") "--watch" config)
+                                 #:environment-variables
+                                 (let ((env (environ)))
+                                   (define (fallback name default)
+                                     (or (getenv name) default))
+                                   (append
+                                    env
+                                    (list
+                                     (string-append "WAYLAND_DISPLAY="
+                                                    (fallback "WAYLAND_DISPLAY" "wayland-1")))))))))
+                   (stop #~(make-kill-destructor)))))))
     %base-home-services)))
